c438bb5898756e8a0a96385e22eb3a693acee88d
Perfilev <perfilevv@student.douglascollege.ca>
2023-08-11 13:52:26 -0700

    Deploy cahnges

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/add" element={<BookForm />} />
+        <Route path="/3380-fe-js" exact element={<Home />} />
+        <Route path="/3380-fe-js/add" element={<BookForm />} />
       </Routes>
     </BrowserRouter>
   );
